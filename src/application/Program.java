7619516f9bae7b577b75801332c185c34f197196
garcia-ant <garcantony19@gmail.com>
2024-08-04 15:56:16 +0100

    findbyId Implemetion

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model_dao.DaoFactory;
 import model_dao.SellerDao;
-import model_entities.Department;
 import model_entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Department department = new Department(1,"Book");
-		System.out.println(department);
-		
-		Seller seller = new Seller(19, "G.A", "ga@gmail.com", new Date(), 3000.00, department);
+
 		SellerDao sellerdao = DaoFactory.creSellerDao();
-		
+		Seller seller = sellerdao.findById(3);
+
 		System.out.println(seller);
-		
+
 	}
 
 }
