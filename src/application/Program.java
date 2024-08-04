4eeb378e4e8618b33555a0459080feea7a35ca47
garcia-ant <garcantony19@gmail.com>
2024-08-04 14:08:28 +0100

    Classe Department

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model_entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department department = new Department(1,"Book");
+		System.out.println(department);
+
+	}
+
+}
